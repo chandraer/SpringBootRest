d55d90b5a55d49b6f6e619fa55570cc25424acec
sairamprasadbandi <180030591@kluniversity.in>
2024-04-25 15:12:20 +0530

    secondcommit

--- src/components/DeleteEx1.jsx
+++ src/components/DeleteEx1.jsx
@@ -3,7 +3,7 @@ import { useState,useEffect } from "react";
 const DeleteEx1=()=>{
     const [num,setNum]=useState(null);
     const delete_data=async ()=>{
-        const response=await axios.delete("http://localhost:2322/delete/16");
+        const response=await axios.delete("http://localhost:2322/delete/1");
         console.log(response);
         const{status}=response;
         setNum(status);
